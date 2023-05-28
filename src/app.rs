ca8bbf5a3c5fcea81a6628046f01c2c5ca97e107
ollielynas <lynasollie@gmail.com>
2023-05-28 12:27:37 +1200

    Added german and french

--- src/app.rs
+++ src/app.rs
@@ -2,7 +2,22 @@
 
 use std::collections::HashMap;
 
+#[derive(serde::Deserialize, serde::Serialize, Clone, Copy, PartialEq, Eq, Hash, Debug)]
+pub enum Language {
+    English,
+    French,
+    German,
+}
 
+impl Language {
+    pub fn to_string(&self) -> String {
+        match self {
+            Language::English => {"English".to_owned()},
+            Language::French => {"Français".to_owned()},
+            Language::German => {"Deutsch".to_owned()},
+        }
+    }
+}
 
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 #[derive(serde::Deserialize, serde::Serialize)]
@@ -12,6 +27,7 @@ struct Word {
     char_count: HashMap<char,i32>,
     score: i32,
     scale: f32,
+    language: Language,
 }
 
 impl Word {
@@ -21,7 +37,7 @@ impl Word {
             let count = char_count.entry(c).or_insert(0);
             *count += 1;
         }
-        Word { word, char_count, score: 0 , scale: 1.0}
+        Word { word, char_count, score: 0 , scale: 1.0, language: Language::English}
     }
 }
 
@@ -32,6 +48,7 @@ impl Default for Word {
             char_count: HashMap::new(),
             score: 0,
             scale: 1.0,
+            language: Language::English,
         }
     }
 }
@@ -46,10 +63,12 @@ pub struct App {
     guessed_letters: Vec<char>,
     current_word: String,
     scale: f32,
+    language: Language,
 }
 
 impl Default for App {
     fn default() -> Self {
+
 
     let mut words = include_str!("words.txt")
         .split_whitespace()
@@ -61,9 +80,9 @@ impl Default for App {
             letter_frequency.insert(letter, letter_frequency.get(&letter).unwrap_or(&0) + count);
         }
     }
+    println!("{:?}", letter_frequency);
 
     for w in 0..words.len() {
-        let mut score = 0;
         words[w].score = words[w].word.chars().map(|c| letter_frequency.get(&c).unwrap_or(&0)).sum::<i32>();
     }
     words.sort_by(|b, a| b.score.cmp(&a.score));
@@ -76,6 +95,7 @@ impl Default for App {
             guessed_letters: vec![],
             current_word: "_ _ _ _ _".to_owned(),
             scale: 1.0,
+            language: Language::English,
         }
     }
 }
@@ -94,6 +114,39 @@ impl App {
         }
 
         Default::default()
+    }
+
+    pub fn update_language(&mut self, language:Language) {
+        self.language = language;
+        let english = include_str!("words.txt");
+        let french = include_str!("french.txt");
+        let german = include_str!("german.txt");
+
+        let mut words = match language {Language::English => {english}, Language::French => {french}, Language::German => {german}}
+        .split_whitespace()
+        .map(|word| Word::new(word.to_string())).collect::<Vec<Word>>();
+
+    let mut letter_frequency = HashMap::new();
+    for w in 0..words.len() {
+        for (letter, count) in words[w].char_count.clone() {
+            letter_frequency.insert(letter, letter_frequency.get(&letter).unwrap_or(&0) + count);
+        }
+    }
+
+    for w in 0..words.len() {
+        words[w].score = words[w].word.chars().map(|c| letter_frequency.get(&c).unwrap_or(&0)).sum::<i32>();
+    }
+    words.sort_by(|b, a| b.score.cmp(&a.score));
+    self.words = words;
+    self.remaining_letters = vec!['a','b','c','d','e','f','g','h','i','j','k','l','m',
+                                    'n','o','p','q','r','s','t','u','v','w','x','y','z'];
+    if language == Language::German {
+        self.remaining_letters.push('ä');
+        self.remaining_letters.push('ö');
+        self.remaining_letters.push('ü');
+        self.remaining_letters.push('é');
+        self.remaining_letters.push('ß');
+    }
     }
 
     pub fn clicked(&mut self, c:char) {
@@ -137,7 +190,7 @@ impl eframe::App for App {
     /// Called each time the UI needs repainting, which may be many times per second.
     /// Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        let Self { words, remaining_letters, guessed_letters, current_word, scale } = self;
+        let Self { words, remaining_letters, guessed_letters, current_word, scale , language} = self;
 
         // Examples of how to create different panels and windows.
         // Pick whichever suits you.
@@ -147,6 +200,7 @@ impl eframe::App for App {
         let mut clicked = None;
         let letters = self.remaining_letters.clone();
         let mut reset = false;
+        let mut update_language = None;
 
 
         egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
@@ -164,17 +218,28 @@ impl eframe::App for App {
                     self.scale = ctx.pixels_per_point()* 1.1;
                     ctx.set_pixels_per_point(self.scale);
                 }
+
+                ui.menu_button(language.to_string(), |ui| {
+                    for i in [Language::English, Language::French, Language::German].iter() {
+                        if ui.button(i.to_string()).clicked() {
+                            update_language = Some(i.clone());
+                            println!("Language changed to {}", i.to_string());
+                        }
+                    }
+                });
+
             });
         });
 
 
-        
+
 
         egui::CentralPanel::default().show(ctx, |ui| {
             ctx.input(|r|{
                 r.keys_down.iter().for_each(|k| {
                     if let Some(c) = k.symbol_or_name().chars().next() {
                         if letters.contains(&c) {
+                        println!("{} was pressed", c);
                         clicked = Some(c);
                     }}
                 });
@@ -218,6 +283,18 @@ impl eframe::App for App {
                                 }
                             }
                         });
+                        if language == &Language::German {
+                            ui.horizontal(|ui| {
+                                ui.add_space(ui.available_width()/2.0-70.0);
+                                for c in "äöüéß".chars() {
+                                    if ui.add_enabled(!guessed_letters.contains(&c),egui::Button::new(c.to_string()).small()).clicked() {
+                                        clicked = Some(c);
+                                    }
+                                }
+                            });
+                        }
+                        
+                        
                     }
                 });
         });
@@ -226,13 +303,18 @@ impl eframe::App for App {
         }
 
         if reset {
-            let d = Self::default();
+            let mut d = Self::default();
+            d.update_language(self.language.clone());
             self.words = d.words;
             self.current_word = d.current_word;
             self.guessed_letters =d.guessed_letters;
             self.remaining_letters = d.remaining_letters;
             
 
+        }
+
+        if let Some(l) = update_language {
+            self.update_language(l);
         }
 
 
